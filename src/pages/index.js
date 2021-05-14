8d673159f00f9294c161b0ec453acc73c8b5678f
Blake Kohler <blakekohler@msn.com>
2021-05-14 11:37:53 -0600

    Cost to build init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,182 +1,263 @@
 import * as React from "react"
+import * as style from "./index.module.css";
+import Header from '../components/header';
+import Footer from "../components/footer";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+import { faWarehouse } from '@fortawesome/free-solid-svg-icons'
+import { faHammer } from '@fortawesome/free-solid-svg-icons'
+import { faBed } from '@fortawesome/free-solid-svg-icons'
+import { faToilet } from '@fortawesome/free-solid-svg-icons'
+import compass from '../images/compass.png';
+import building from '../images/build.png';
+import house from '../images/nearme.png';
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
 
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
 
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
 
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
-
-// markup
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
+    <main >
       <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+      <Header></Header>
+     <div className={style.darkHouse}>
+       <div className={style.heroText}>
+         <h1>Calculate Your Dream</h1>
+         <h3>Use our <span className={style.free}>free</span> construction calculators to estimate the cost for your next project</h3>
+       </div>
+     </div>
+     <div className={style.calcHolder}>
+       <a className={style.calc}>
+         <div className={style.iconHolder}>
+         <div className={style.calcIcon}>
+         <FontAwesomeIcon icon={faHome} />
+         </div>
+         </div>
+        
+         <div className={style.calcHeaderRow}>
+           <h3 className={style.calcHeader}>Housing Calculator</h3>
+         </div>
+         <div className={style.calcInfo}>
+         Determine the cost to build your next home
+         </div>
+         <div className={style.calcActionRow}>
+          <div className={style.calcAction}>Calculate House Cost</div>
+         </div>
+       </a>
+      
+       <a className={style.calc}>
+         <div className={style.iconHolder}>
+         <div className={style.calcIcon}>
+         <FontAwesomeIcon icon={faWarehouse} />
+         </div>
+         </div>
+        
+         <div className={style.calcHeaderRow}>
+           <h3 className={style.calcHeader}>Garage Calculator</h3>
+         </div>
+         <div className={style.calcInfo}>
+         Determine the cost to build a garage
+         </div>
+         <div className={style.calcActionRow}>
+          <div className={style.calcAction}>Calculate Garage Cost</div>
+         </div>
+       </a>
+
+
+       <a className={style.calc}>
+         <div className={style.iconHolder}>
+         <div className={style.calcIcon}>
+         <FontAwesomeIcon icon={faHammer} />
+         </div>
+         </div>
+        
+         <div className={style.calcHeaderRow}>
+           <h3 className={style.calcHeader}>Professional Estiamte Tool</h3>
+         </div>
+         <div className={style.calcInfo}>
+         Are you a pro? Use our professional estimate tool to give quality estimates.
+         </div>
+         <div className={style.calcActionRow}>
+          <div className={style.calcAction}>Start an Estimate</div>
+         </div>
+       </a>
+     </div>
+
+<section>
+<div className={style.ourPlans}>
+  <h2 className={style.sectionHeader}>  《 <span className={style.white}>Our Top Plans</span>  》</h2>
+  <div className={style.planList}>
+<div className={style.plan}>
+<img className={style.planPic} src="https://www.crookstondesigns.com/plan_pictures/12050.00/picture1.jpg"/>
+<div className={style.details}>
+<h4>Rambled</h4>
+<div>Square Footage: 2338 sf</div>
+<div>Bedrooms: 4</div>
+<div>Baths: 3</div>
+</div>
+<a className={style.viewPlan}>View Plan In Calculator</a>
+</div>
+
+<div className={style.plan}>
+<img className={style.planPic} src="https://www.crookstondesigns.com/plan_pictures/12050.00/picture1.jpg"/>
+<div className={style.details}>
+<h4>Rambled</h4>
+<div>Square Footage: 2338 sf</div>
+<div>  <FontAwesomeIcon icon={faBed} /> Bedrooms: 4</div>
+<div>  <FontAwesomeIcon icon={faToilet} />Baths: 3</div>
+</div>
+<a className={style.viewPlan}>View Plan In Calculator</a>
+</div>
+
+
+
+<div className={style.plan}>
+<img className={style.planPic} src="https://www.crookstondesigns.com/plan_pictures/12050.00/picture1.jpg"/>
+<div className={style.details}>
+<h4>Rambled</h4>
+<div>Square Footage: 2338 sf</div>
+<div>  <FontAwesomeIcon icon={faBed} /> Bedrooms: 4</div>
+<div>  <FontAwesomeIcon icon={faToilet} />Baths: 3</div>
+</div>
+<a className={style.viewPlan}>View Plan In Calculator</a>
+</div>
+
+<div className={style.plan}>
+<img className={style.planPic} src="https://www.crookstondesigns.com/plan_pictures/12050.00/picture1.jpg"/>
+<div className={style.details}>
+<h4>Rambled</h4>
+<div>Square Footage: 2338 sf</div>
+<div className={style.bed}>  <FontAwesomeIcon icon={faBed} /> Bedrooms: 4</div>
+<div className={style.bath}>  <FontAwesomeIcon icon={faToilet} />Baths: 3</div>
+</div>
+<a className={style.viewPlan}>View Plan In Calculator</a>
+</div>
+
+
+  </div>
+</div>
+</section>
+
+
+{/* Services Near Me Section  */}
+<section>
+  <div className={style.services}>
+    <div className={style.serviceRow}>
+    <div className={style.service}>
+      <div className={style.serviceIcon}>
+      <img src={building} />
+      </div>
+      <h2 className={style.serviceHeader}>Find Builders</h2>
+      <div className={style.serviceDescription}>View reviews and information about builders near you.</div>
+      <a className={style.serviceLink}>View Builders</a>
+          </div>
+
+          <div className={style.service}>
+      <div className={style.serviceIcon}>
+      <img src={house} />
+      </div>
+      <h2 className={style.serviceHeader}>Find Homes</h2>
+      <div className={style.serviceDescription}>View listing of new homes near me</div>
+      <a className={style.serviceLink}>See Homes</a>
+          </div>
+
+          <div className={style.service}>
+      <div className={style.serviceIcon}>
+      <img src={compass} />
+      </div>
+      <h2 className={style.serviceHeader}>Find Designers</h2>
+      <div className={style.serviceDescription}>View reviews and information about designers near you.</div>
+      <a className={style.serviceLink}>View Designers</a>
+          </div>
+    </div>
+   
+  </div>
+</section>
+
+
+
+
+
+{/* Blog Section  */}
+<section>
+  <div className={style.blogHolder}>
+    <h2 className={style.sectionHeader}>《 <span className={style.yellow}>From Our Blog</span> 》</h2>
+    <div className={style.blogRow}>
+      <div className={style.blog}>
+        <div className={style.blogImg}>
+          <img src="https://h7f7z2r7.stackpathcdn.com/sites/default/files/images/articles/housing-ap_1.jpg"/>
+          <div className={style.blogDate}>
+            <div className={style.day}></div> 
+            <div className={style.month}></div>
+          </div>
+        </div>
+        <div className={style.blogInfo}>
+        <div className={style.blogTitle}>
+          <h3>When it comes to your house, don't mess wit hthe rest, trust the best</h3>
+        </div>
+        <div className={style.author}>By Blake Kohler</div>
+        <div className={style.description}>Same as saying through shrinking from toil and pain these cases perfectly simple.</div>
+        <div className={style.readMore}><a>Read More ›</a></div>
+        </div>
+      </div>
+
+      <div className={style.blog}>
+        <div className={style.blogImg}>
+          <img src="https://h7f7z2r7.stackpathcdn.com/sites/default/files/images/articles/housing-ap_1.jpg"/>
+          <div className={style.blogDate}>
+            <div className={style.day}></div> 
+            <div className={style.month}></div>
+          </div>
+        </div>
+        <div className={style.blogInfo}>
+        <div className={style.blogTitle}>
+          <h3>When it comes to your house, don't mess wit hthe rest, trust the best</h3>
+        </div>
+        <div className={style.author}>By Blake Kohler</div>
+        <div className={style.description}>Same as saying through shrinking from toil and pain these cases perfectly simple.</div>
+        <div className={style.readMore}><a>Read More ›</a></div>
+        </div>
+      </div>
+
+
+      <div className={style.blog}>
+        <div className={style.blogImg}>
+          <img src="https://h7f7z2r7.stackpathcdn.com/sites/default/files/images/articles/housing-ap_1.jpg"/>
+          <div className={style.blogDate}>
+            <div className={style.day}></div> 
+            <div className={style.month}></div>
+          </div>
+        </div>
+        <div className={style.blogInfo}>
+        <div className={style.blogTitle}>
+          <h3>When it comes to your house, don't mess wit hthe rest, trust the best</h3>
+        </div>
+        <div className={style.author}>By Blake Kohler</div>
+        <div className={style.description}>Same as saying through shrinking from toil and pain these cases perfectly simple.</div>
+        <div className={style.readMore}><a>Read More ›</a></div>
+        </div>
+      </div>
+
+      <div className={style.blog}>
+        <div className={style.blogImg}>
+          <img src="https://h7f7z2r7.stackpathcdn.com/sites/default/files/images/articles/housing-ap_1.jpg"/>
+        </div>
+        <div className={style.blogInfo}>
+        <div className={style.blogTitle}>
+          <h3>When it comes to your house, don't mess wit hthe rest, trust the best</h3>
+        </div>
+        <div className={style.author}>By Blake Kohler</div>
+        <div className={style.description}>Same as saying through shrinking from toil and pain these cases perfectly simple.</div>
+        <div className={style.readMore}><a>Read More ›</a></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<Footer></Footer>
     </main>
   )
 }
