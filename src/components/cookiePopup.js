58480c73da09dbaaab1344e2d17a62101ab06aee
Blake Kohler <blakekohler@msn.com>
2021-06-14 21:03:35 -0600

    does this work on netlify?

---
+++ src/components/cookiePopup.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react'
+import style from "./cookiePopup.module.css"
+const CookiePopup  = () => {
+    const [hide, setHide] = useState(true);
+    const handleClose = () => {
+      localStorage.setItem("cookieAgree", true);
+      setHide(true);
+    }
+    useEffect(() => {
+      if (localStorage.getItem("cookieAgree") == true) {
+        setHide(true)
+      }
+      else{
+        setHide(false)
+      }
+    }, [])
+    return (
+      <>
+     <div className={style.modalbackground} style={{ display: hide ? 'none' : 'flex'  }}>
+       <div className={style.modalContent}>
+       This website uses cookies in order to offer you the most relevant information. Please accept cookies for optimal performance.
+       <a className={style.decline} href="https://www.google.com">Decline</a>  <div className={style.accept} onClick={handleClose} href="">Accept</div>
+       </div>
+     </div>
+      </>
+    );
+};
+export default CookiePopup
